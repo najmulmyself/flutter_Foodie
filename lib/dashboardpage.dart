7b11a5178b0d97315e21d8ae8548882989856d3d
najmulmyself <najmul.myself@gmail.com>
2022-07-02 16:40:35 +0600

    added textfield

--- lib/dashboardpage.dart
+++ lib/dashboardpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DashboardPage extends StatefulWidget {
   DashboardPage({Key? key}) : super(key: key);
@@ -10,13 +11,19 @@ class DashboardPage extends StatefulWidget {
 class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-          Icon(Icons.menu, color: Colors.black),
-              Container(
-                height: 50.0,
-                width: 50.0,
-                decoration: BoxDecoration(
+    return Scaffold(
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Icon(Icons.menu, color: Colors.black),
+                Container(
+                  height: 50.0,
+                  width: 50.0,
+                  decoration: BoxDecoration(
                     boxShadow: [
                       BoxShadow(
                           color: Colors.grey.withOpacity(0.3),
@@ -28,9 +35,47 @@ class _DashboardPageState extends State<DashboardPage> {
                     shape: BoxShape.circle,
                     image: DecorationImage(
                         image: AssetImage('assets/tuxedo.png'),
-                        fit: BoxFit.contain)),
-              )
-      ],
+                        fit: BoxFit.contain),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0),
+            child: Text(
+              'SEARCH FOR',
+              style: GoogleFonts.notoSans(
+                  fontWeight: FontWeight.w800, fontSize: 27.0),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0),
+            child: Text(
+              'RECIPES',
+              style: GoogleFonts.notoSans(
+                  fontWeight: FontWeight.w800, fontSize: 27.0),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 15.0, right: 15.0),
+            child: Container(
+              padding: EdgeInsets.only(left: 5.0),
+              decoration: BoxDecoration(
+                  color: Colors.grey.withOpacity(0.1),
+                  borderRadius: BorderRadius.circular(10.0)),
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: 'Search',
+                    hintStyle: GoogleFonts.notoSans(fontSize: 14.0),
+                    border: InputBorder.none,
+                    fillColor: Colors.grey.withOpacity(0.5),
+                    prefixIcon: Icon(Icons.search, color: Colors.grey)),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
