e7e7fdd8b7a9b9fee5a36f48c77b44a5e7f34d40
najmulmyself <najmul.myself@gmail.com>
2022-07-02 14:17:02 +0600

    project init

---
+++ lib/dashboardpage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DashboardPage extends StatefulWidget {
+  DashboardPage({Key? key}) : super(key: key);
+
+  @override
+  State<DashboardPage> createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+      ),
+    );
+  }
+}
